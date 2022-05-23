064da6ef00b06d7d3be1cd69fbf172130fd12c08
hca <hcalsos@gmail.com>
2022-05-23 08:53:27 +0200

    Split firehose subscription for older topics not using our internal module.

--- sns-firehose-subscription/main.tf
+++ sns-firehose-subscription/main.tf
@@ -1,17 +1,14 @@
 data "aws_kinesis_firehose_delivery_stream" "firehose_delivery_stream" {
-  count  = var.enable_logging_of_published_messages ? 1 : 0
   name = var.kinesis_firehose_delivery_stream_name
 }
 
 data "aws_iam_role" "sns_to_kinesis_role" {
-  count  = var.enable_logging_of_published_messages ? 1 : 0
   name = var.sns_to_kinesis_role_name
 }
 
 resource "aws_sns_topic_subscription" "firehose_topic_subscription" {
-  count  = var.enable_logging_of_published_messages ? 1 : 0
-  endpoint  = data.aws_kinesis_firehose_delivery_stream.firehose_delivery_stream[0].arn
-  protocol  = "firehose"
+  endpoint              = data.aws_kinesis_firehose_delivery_stream.firehose_delivery_stream[0].arn
+  protocol              = "firehose"
   subscription_role_arn = data.aws_iam_role.sns_to_kinesis_role[0].arn
-  topic_arn = aws_sns_topic.sns_topic.arn
+  topic_arn             = var.sns_topic_arn
 }
--- sns-topic/variables.tf
+++ sns-topic/variables.tf
@@ -18,19 +18,3 @@ variable "external_subscribers" {
   description = "A list of account numbers that are allowed to create topic subscriptions"
   type        = list(string)
 }
-
-variable "enable_logging_of_published_messages" {
-  description = "Enable logging of published messages to s3 using firehose delivery stream"
-  type        = bool
-  default     = false
-}
-
-variable "kinesis_firehose_delivery_stream_name" {
-  type    = string
-  default = ""
-}
-
-variable "sns_to_kinesis_role_name" {
-  type    = string
-  default = ""
-}
