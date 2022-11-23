6f798f015bf38cd23bf87f57e4ca0fb44848ca2f
asmundh <aasmund.haugse@gmail.com>
2022-11-23 10:05:21 +0100

    TCONTROL-1441: Add S3-bucket for large SNS messages
    Adds optional resources to create an S3 bucket for large SNS message payloads.
    Configures read access to the S3-bucket for external subscribers.

--- sns-topic/variables.tf
+++ sns-topic/variables.tf
@@ -18,3 +18,13 @@ variable "external_subscribers" {
   description = "A list of account numbers that are allowed to create topic subscriptions"
   type        = list(string)
 }
+
+variable "create_payload_bucket" {
+  description = "Whether to create an S3-bucket for large messages."
+  type = bool
+}
+
+variable "payload_bucket_name" {
+  description = "Name of the SNS payload S3-bucket."
+  type = string
+}
