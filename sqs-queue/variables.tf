453a5f1ca32d3f01600c91084dc76dc45ca08f62
Benjamin Sjølander <17160920+putetrekk@users.noreply.github.com>
2023-04-24 11:25:09 +0200

    Add support for custom DLQ message retention (#17)
    * Add support for custom DLQ message retention
    
    * Remove default retention period for DLQ
    
    * Coalesce retention period with tenfold if not present
    
    * Default dlq retention to null

--- sqs-queue/variables.tf
+++ sqs-queue/variables.tf
@@ -12,6 +12,10 @@ variable "message_retention_seconds" {
   type    = number
   default = 86400
 }
+variable "message_retention_seconds_dlq" {
+  type    = number
+  default = null
+}
 variable "receive_wait_time_seconds" {
   type    = number
   default = 20
