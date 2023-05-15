4a9083ee5847fca8b5f61c8fe27429d17169883a
hca <hcalsos@gmail.com>
2023-05-15 10:47:05 +0200

    TCONTROL-1646 use index of dlq resource

--- sqs-queue/alarms.tf
+++ sqs-queue/alarms.tf
@@ -4,17 +4,17 @@
 resource "aws_cloudwatch_metric_alarm" "dlq_unhealty" {
   count               = var.enable_dlq_alarm ? 1 : 0
   metric_name         = "NumberOfMessagesReceived"
-  alarm_name          = "${aws_sqs_queue.sqs_queue_dlq.name} contains unprocessed messages"
+  alarm_name          = "${aws_sqs_queue.sqs_queue_dlq[0].name} contains unprocessed messages"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 1
   namespace           = "AWS/SQS"
   dimensions = {
-    QueueName = aws_sqs_queue.sqs_queue_dlq.name
+    QueueName = aws_sqs_queue.sqs_queue_dlq[0].name
   }
   period            = 60
   statistic         = "Sum"
-  alarm_description = "${aws_sqs_queue.sqs_queue_dlq.name} contains unprocessed messages"
+  alarm_description = "${aws_sqs_queue.sqs_queue_dlq[0].name} contains unprocessed messages"
   tags              = var.tags
   alarm_actions     = [var.alarm_topic_arn]
   ok_actions        = [var.alarm_topic_arn]
