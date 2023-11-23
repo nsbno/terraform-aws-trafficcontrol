e41fc14d9522a6a9f7324fc4f8ba6172379fbef7
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2023-11-23 11:59:22 +0100

    Expose DLQ attributes as outputs from sns-topic-subscription

--- sns-topic-subscription/outputs.tf
+++ sns-topic-subscription/outputs.tf
@@ -12,3 +12,18 @@ output "sqs_queue_name" {
   description = "The Amazon SQS name created in this module"
   value       = module.sqs_queue.sqs_queue_name
 }
+
+output "sqs_queue_dlq_arn" {
+  description = "The Amazon SQS DLQ arn created in this module"
+  value       = module.sqs_queue.sqs_queue_dlq_arn
+}
+
+output "sqs_queue_dlq_id" {
+  description = "The Amazon SQS DLQ id created in this module"
+  value       = module.sqs_queue.sqs_queue_dlq_id
+}
+
+output "sqs_queue_dlq_name" {
+  description = "The Amazon SQS DLQ name created in this module"
+  value       = module.sqs_queue.sqs_queue_dlq_name
+}
