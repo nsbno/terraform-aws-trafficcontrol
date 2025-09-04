3a813bc811ed7ca2819f2e07692c6a820276e521
Wictor Zhao <wictor_x@hotmail.com>
2025-09-04 12:05:57 +0200

    Revert "Add missing principals definition"
    This reverts commit 69eccf561f2b81e06d17752c80025ef897b063a9.

--- oidc-role/policies.tf
+++ oidc-role/policies.tf
@@ -70,12 +70,8 @@ data "aws_iam_policy_document" "allow_publish_to_deployment_reporter" {
   version = "2012-10-17"
 
   statement {
-    effect  = "Allow"
-    actions = ["sns:Publish"]
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.oidc_assume_role.arn]
-    }
-    resources = ["*"]
+	effect    = "Allow"
+	actions   = ["sns:Publish"]
+	resources = ["*"]
   }
 }
